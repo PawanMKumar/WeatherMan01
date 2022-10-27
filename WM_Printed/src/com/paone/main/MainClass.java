a6fce231687e5c58b23db3344d17dc5a06341bb9
pawan <pawan@PavanMK>
2022-10-27 11:43:38 +0530

    Hello World Commit

---
+++ WM_Printed/src/com/paone/main/MainClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.paone.main;
+
+/**
+ * @author pawan
+ *
+ */
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!!");
+	}
+
+}
